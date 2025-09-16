52a97794f5799bfce4720a3c0e2c8e9a6a79d3eb
zatsepin stepan <zatsepin.sa@phystech.edu>
2025-09-16 17:49:19 +0300

    some shi

---
+++ get-led/digital-blink.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as gp
+import time as tm
+
+gp.setmode(gp.BCM)
+
+led=26
+
+gp.setup(led, gp.OUT)
+
+state=0
+
+period=1.0
+
+while True:
+    gp.output(led, state)
+    tm.sleep(period)
+    state = (state+1)%2
